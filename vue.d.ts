0b0269f5a8869cb7d63c0d23af63490184bf051c
Audioaxel <alex@audiolarks.com>
2024-03-14 18:43:57 +0100

    m

--- vue.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="vite/client" />
-/// <reference types="vite/types" />
- 
-declare module '*.vue' {
-  import { DefineComponent } from 'vue';
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
-}
